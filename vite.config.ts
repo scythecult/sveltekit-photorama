74cc050cb6148f8ec18a60021fb2f61aa8946026
scythecult <scythecult@yandex.ru>
2025-04-01 21:03:38 +0300

    [JS-9] Updated deps and configs
    - Updated nodejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [sveltekit(), paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' })],
+  plugins: [sveltekit(), paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })],
 
   test: { include: ['tests/unit/**/*.{test,spec}.{js,ts}'] },
 });
