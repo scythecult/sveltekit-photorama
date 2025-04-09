e7fb5c39e2d4ced9d0fd5b7f89830743e54a6c52
scythecult <scythecult@yandex.ru>
2025-04-09 21:53:34 +0300

    [JS-10] Implemented components

---
+++ src/lib/components/loader/Loader.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import './styles.css';
+  import { navigating } from '$app/state';
+
+  type LoaderProps = {
+    loadingType?: typeof navigating.type;
+  };
+
+  const { loadingType = 'goto' }: LoaderProps = $props();
+</script>
+
+{#if navigating.type === loadingType}
+  <div class="progress"></div>
+{/if}
